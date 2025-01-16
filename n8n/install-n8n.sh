f8b73d76bea89795b744c926fd077a2111640c17
Dung <dungxtd20@gmail.com>
2025-01-16 11:32:23 +0700

    update credential

--- n8n/install-n8n.sh
+++ n8n/install-n8n.sh
@@ -1,3 +1,3 @@
 # helm install -f postgresdb.yaml postgresdb oci://registry-1.docker.io/bitnamicharts/postgresql
-
+helm install -f redis.yaml redis oci://registry-1.docker.io/bitnamicharts/redis 
 helm install -f n8n.yaml n8n oci://8gears.container-registry.com/library/n8n
